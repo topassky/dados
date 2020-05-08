7d63da083953281adc587f0c6193f356ddfa5fb3
topassky <juan.orozcoco@gmail.com>
2020-05-08 09:12:36 -0500

    first commit

---
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
